f17ab3c1f43aaf1122c52b8ae74ac4daaf391e34
Magus <mfail792@gmail.com>
2019-11-21 17:04:14 -0600

    Updates

---
+++ precache-manifest.4935e2ec3bf0fbc6569c0ca62bc04fbb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "072751495a5cb539acd18211f363ecc1",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "ca8a9103c24164e93d56",
+    "url": "/ClickyGame/static/css/main.8160beae.chunk.css"
+  },
+  {
+    "revision": "66baef837960f0c64e19",
+    "url": "/ClickyGame/static/js/2.e640b08d.chunk.js"
+  },
+  {
+    "revision": "ca8a9103c24164e93d56",
+    "url": "/ClickyGame/static/js/main.c4733599.chunk.js"
+  },
+  {
+    "revision": "e0c4ecab753330125d7b",
+    "url": "/ClickyGame/static/js/runtime-main.ec2735bc.js"
+  }
+]);
